cd10559ddfa28c7838b4767afbff49a75dbdb23f
zhengyuanhong <1713639570@qq.com>
2020-03-02 20:17:11 +0800

    fix admin

--- index/admin.py
+++ index/admin.py
@@ -4,7 +4,7 @@ from .models import category,comments,article,fineLink
 @admin.register(category)
 class categoryAdmin(admin.ModelAdmin):
     list_display = ('id','name','categoryId','updateTime','createTime')
-
+    list_editable = ('name',)
 @admin.register(comments)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('id','aid','comm_uid','comm_content','updateTime','createTime')
@@ -13,7 +13,7 @@ class CommentAdmin(admin.ModelAdmin):
 @admin.register(article)
 class articleAdmin(admin.ModelAdmin):
     list_display=('id','title','uid','content','is_show','updateTime','createTime')
-    
+    list_display_links = ('title','content')
 #修改网页title和站点header。
 admin.site.site_title = "X社区后台管理"
 admin.site.site_header = "X社区"
