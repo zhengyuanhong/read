8d4a24a86c576b523ee540c402fb23e1d74995c3
zhengyuanhong <1713639570@qq.com>
2020-03-04 18:09:32 +0800

    fix reset password

--- account/views.py
+++ account/views.py
@@ -66,21 +66,18 @@ def accountLogin(request):
             try:
                 user = siteUser.objects.get(email=email)
             except Exception:
-                # return render(request,'account/login.html',{'message':'邮箱不存在'})
                 return JsonResponse({'code': 201, 'msg': '邮箱不存在'})
 
             res = authenticate(username=user.username, password=password)
             # 查看是否有此用户
             if res is not None:
                 if not res.is_verif:
-                    # return render(request,'account/login.html',{'message':'账号未激活','flag':True})
                     return JsonResponse({'code': 202, 'msg': '账号未激活'})      
                 else:
                     # 登陆
                     login(request, user=res)
                     return JsonResponse({'code': 200, 'msg': '登陆成功'})   
             else:
-                # return render(request,'account/login.html',{'message':'用户名或密码错误'})
                 return JsonResponse({'code': 201, 'msg': '用户名或密码错误'})
     return render(request, 'account/login.html')
 
@@ -196,19 +193,47 @@ def setPassword(request):
 # 重置密码
 def resetPassword(request):
     if request.method == 'GET':
-        userinfo = de_token(request.GET.get('token'))
+        token = request.GET.get('token')
+        userinfo = de_token(token)
         if not userinfo:
-            return render(request, 'account/reset_pass_sucess.html', {'message': '验证已过期'})
+            return render(request, 'account/reset_pass_tip.html', {'message': '验证已过期'})
         else:
-            new_pass = random_str(10)
-            siteUser.objects.filter(email=userinfo['email']).update(password=make_password(new_pass))
-            res = '您重置的新密码：'+  new_pass
-            return render(request,'account/reset_pass_sucess.html',{'message':'密码重置成功','res':res}) 
+            return render(request,'account/reset_pass_page.html',{'message':token}) 
+
+    if request.method == 'POST':
+        newpassword = request.POST.get('newpassword')
+        repassword = request.POST.get('repassword')
+        key = request.POST.get('key',None)
+
+        if not key:
+            return JsonResponse({'code': 201, 'msg': '非法请求'})
+
+        userinfo = de_token(key)
+        if not userinfo:
+            return JsonResponse({'code': 201, 'msg': '验证链接已过期，请重新获取'})
+        
+        if not (len(newpassword) >= 8):
+            return JsonResponse({'code': 201, 'msg': '密码长度不够'})
+
+        # 判断密码是否重复
+        if newpassword != repassword:
+            return JsonResponse({'code': 201, 'msg': '两次密码输入不正确'})
+
+        user = siteUser.objects.filter(email=userinfo['email'])
+        userobj =  user.first()
+        if not userobj.is_verif:
+            return JsonResponse({'code': 201, 'msg': '账号未激活'})      
+
+        if not userobj.is_active:
+            return JsonResponse({'code': 201, 'msg': '该账号已被禁止登陆'})      
+
+        user.update(password=make_password(newpassword))
+        return JsonResponse({'code': 200, 'msg': '修改成功'})
 
 # 发送重置密码链接
 def sendResetPassword(request):
     if request.method == 'GET':
-        return render(request,'account/reset_pass.html',{'flag':True,'tip':'请输入正确的邮箱地址'})
+        return render(request,'account/reset_pass.html',{'flag':True,'tip':'请输入正确的邮箱地址（发送邮箱过程可能会有点长，请耐心等待）'})
     if request.method == 'POST':
         email = request.POST.get('email')
 
