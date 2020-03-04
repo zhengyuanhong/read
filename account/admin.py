a862c721c3a8ffa19af45701f134a0dbc187b2b5
zhengyuanhong <1713639570@qq.com>
2020-03-04 14:20:24 +0800

    add search

--- account/admin.py
+++ account/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import siteUser
 
+
 @admin.register(siteUser)
 class AuthAdmin(admin.ModelAdmin):
-    list_display = ('id','username','jifen','date_joined')
+    list_display = ('id', 'username', 'jifen', 'date_joined')
+    search_fields = ('username', 'email')
