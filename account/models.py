a5fa01963edb5320c2fcca8ddbe1acdead880d11
zhengyuanhong <1713639570@qq.com>
2020-03-19 23:24:40 +0800

    fix

--- account/models.py
+++ account/models.py
@@ -5,7 +5,7 @@ class siteUser(AbstractUser):
     sex = models.CharField('性别',max_length=5,choices=(('male','男'),('female','女')),default = 'male')
     jifen = models.IntegerField('积分',default=100)
     qianming = models.CharField('签名',max_length=20,default='这位有点懒')
-    avatar = models.URLField('链接',null=True)
+    avatar = models.URLField('头像链接',null=True,blank=True)
     is_verif = models.BooleanField('是否验证',default=False)
 
     
